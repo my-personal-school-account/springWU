b843f84f0d285bca3d80fd28953be8928e6c5dea
rayererer <119081004+rayererer@users.noreply.github.com>
2026-03-11 13:14:55 +0100

    Fixed so the nav is togglable on mobile aswell and added a fade-layer that you can click to close the nav. Also fixed delay of animation start for the sidebutton too

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 const togglableNavString = "nav.togglable";
 const sideButtonArrowString = "button.nav-button.side-button";
+const fadeLayerString = "div.fade-layer";
 
 fetch('header.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById("header-template").innerHTML = html;
-    document.querySelectorAll("button.nav-button").forEach((button) => button.addEventListener("click", toggleMenu));
+    document.querySelectorAll(".nav-button").forEach((button) => button.addEventListener("click", toggleMenu));
 
     let togglableNav = document.querySelector(togglableNavString);
+    let sideButton = document.querySelector(sideButtonArrowString);
 
     if (window.innerWidth >= 1000) {
       togglableNav.classList.add("show");
+      document.querySelector(fadeLayerString).classList.add("show");
+      sideButton.classList.add("is-left");
     }
 
     requestAnimationFrame(() => {
       requestAnimationFrame(() => {
         togglableNav.classList.add("with-transitions");
+        sideButton.classList.add("with-transitions");
       });
     });
 
@@ -23,6 +28,7 @@ fetch('header.html')
 
 function toggleMenu() {
   document.querySelector(togglableNavString).classList.toggle("show");
+  document.querySelector(fadeLayerString).classList.toggle("show");
 
   document.querySelector(sideButtonArrowString).classList.toggle("is-left");
 }
