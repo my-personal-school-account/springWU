241ffa76cdd661c6e47f51d8b69a531818271e5d
rayererer <119081004+rayererer@users.noreply.github.com>
2026-03-11 11:52:07 +0100

    Fixed so the nav is togglable on desktop with a button that flows with the menu and is correctly placed

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
+const togglableNavString = "nav.togglable";
+const sideButtonArrowString = "button.nav-button.side-button";
+
 fetch('header.html')
   .then(res => res.text())
   .then(html => {
-    document.getElementById('header-template').innerHTML = html;
-    document.querySelector("button.nav-button").addEventListener("click", toggleMenu);
+    document.getElementById("header-template").innerHTML = html;
+    document.querySelectorAll("button.nav-button").forEach((button) => button.addEventListener("click", toggleMenu));
+
+    let togglableNav = document.querySelector(togglableNavString);
+
     if (window.innerWidth >= 1000) {
-      document.querySelector("nav.togglable").classList.add("show");
+      togglableNav.classList.add("show");
     }
+
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        togglableNav.classList.add("with-transitions");
+      });
+    });
+
   });
 
 function toggleMenu() {
-  document.querySelector("nav.togglable").classList.toggle("show");
+  document.querySelector(togglableNavString).classList.toggle("show");
+
+  document.querySelector(sideButtonArrowString).classList.toggle("is-left");
 }
