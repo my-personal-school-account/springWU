f6b80cf95d36815dcc186cec1c16b14cfc412f5f
rayererer <119081004+rayererer@users.noreply.github.com>
2026-04-15 19:02:32 +0200

    Added a template for the footer, fixed so template logic is called separately and everything is not nested inside it for no reason. Also added images, interests and links pages and fixed link colors according to how I want them currently

--- js/main.js
+++ js/main.js
@@ -8,42 +8,48 @@ fetch('header.html')
   .then(res => res.text())
   .then(html => {
     document.getElementById("header-template").innerHTML = html;
-    document.querySelectorAll(".nav-button").forEach((button) => button.addEventListener("click", toggleMenu));
+  });
 
-    let togglableNav = document.querySelector(togglableNavString);
-    let sideButton = document.querySelector(sideButtonArrowString);
+fetch('footer.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("footer-template").innerHTML = html;
+  });
 
-    if (window.innerWidth >= 1000) {
-      togglableNav.classList.add("show");
-      document.querySelector(fadeLayerString).classList.add("show");
-      sideButton.classList.add("is-left");
-    }
+document.querySelectorAll(".nav-button").forEach((button) => button.addEventListener("click", toggleMenu));
 
-    let divTree = document.querySelector(divTreeString);
-    let articleHeaders = document.querySelectorAll(articleHeadingString);
-    articleHeaders.forEach((header) => {
+let togglableNav = document.querySelector(togglableNavString);
+let sideButton = document.querySelector(sideButtonArrowString);
 
-      let heading_number = header.tagName.charAt(1);
-      let padding_text = (heading_number - 1) * 1.5 + "em";
+if (window.innerWidth >= 1000) {
+  togglableNav.classList.add("show");
+  document.querySelector(fadeLayerString).classList.add("show");
+  sideButton.classList.add("is-left");
+}
 
-      let text = header.textContent;
-      header.id = text;
-      divTree.insertAdjacentHTML("beforeend", `
+let divTree = document.querySelector(divTreeString);
+let articleHeaders = document.querySelectorAll(articleHeadingString);
+articleHeaders.forEach((header) => {
+
+  let heading_number = header.tagName.charAt(1);
+  let padding_text = (heading_number - 1) * 1.5 + "em";
+
+  let text = header.textContent;
+  header.id = text;
+  divTree.insertAdjacentHTML("beforeend", `
           <li style="margin-left:${padding_text};">
             <a href="#${text}">${text}</a>
           </li>
         `)
 
-    });
+});
 
-    requestAnimationFrame(() => {
-      requestAnimationFrame(() => {
-        togglableNav.classList.add("with-transitions");
-        sideButton.classList.add("with-transitions");
-      });
-    });
-
+requestAnimationFrame(() => {
+  requestAnimationFrame(() => {
+    togglableNav.classList.add("with-transitions");
+    sideButton.classList.add("with-transitions");
   });
+});
 
 function toggleMenu() {
   document.querySelector(togglableNavString).classList.toggle("show");
